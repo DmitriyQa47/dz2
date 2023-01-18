cbf13ee9cd188dff9a4871c8c3522d1475965ddc
Dmitriy Sikorskiy <sikorskiy_1997@list.ru>
2023-01-18 16:30:41 +0300

    ititial commit

---
+++ src/main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+        int Rubles = 5000;
+        int percentInRubles = 20;
+        int bonus = Rubles / percentInRubles;
+        System.out.println("Your bonus is:"+ bonus +" miles");
+    }
+}
